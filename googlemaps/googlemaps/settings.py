7829dc98cb03ea60471a4b57dc21a85933eb5cc5
denischmkh <avraam.l2004@gmail.com>
2025-04-04 17:32:57 +0300

    fix

--- googlemaps/googlemaps/settings.py
+++ googlemaps/googlemaps/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +133,7 @@ import os
 STATIC_URL = '/static/'  # Это стандартное значение для доступа к статическим файлам
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Путь к папке для собранных статических файлов
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
