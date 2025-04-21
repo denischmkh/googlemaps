6c46351434ccdff53e349f07ddd1f06acbcc11a3
denischmkh <avraam.l2004@gmail.com>
2025-04-21 12:17:42 +0300

    remake main menu

--- googlemaps/googlemaps/settings.py
+++ googlemaps/googlemaps/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'googlemaps.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-#
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
