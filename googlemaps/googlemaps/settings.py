a183c9f61e8e3db690a28578b59eece065870137
denischmkh <avraam.l2004@gmail.com>
2025-04-21 10:16:17 +0300

    remake main menu

--- googlemaps/googlemaps/settings.py
+++ googlemaps/googlemaps/settings.py
@@ -84,28 +84,28 @@ WSGI_APPLICATION = 'googlemaps.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'google_maps_crm_db',
-#         'USER': 'admin',
-#         'PASSWORD': 'gthyfrde',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'google_maps_crm_db',
-        'USER': 'postgres',
-        'PASSWORD': 'denis2004',
+        'USER': 'admin',
+        'PASSWORD': 'gthyfrde',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'google_maps_crm_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'denis2004',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
